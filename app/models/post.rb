6644cd21239bbbd76a9fb0dc95c08bd2361c3f0a
sunkencity999 <theroyalart@gmail.com>
2016-06-23 14:23:43 -0700

    fix for old scope in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,22 +2,22 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :list
 
-  #use this to associate with rake task in schedule.rb to automate 
-  #destruction of completed posts. 
+  #use this to associate with rake task in schedule.rb to automate
+  #destruction of completed posts.
 
   scope :completed, -> { where(completed: true) }
 
 
   default_scope { order('created_at DESC')}
-  
+
   #this scope used to associate ":old" with Post, and enable rake automation
-  #in config/schedule.rb and destroy old posts after a week's time. 
-  
-  scope :old, -> { where(["created_at >= ?", 7.days.ago]) }
+  #in config/schedule.rb and destroy old posts after a week's time.
+
+  scope :old, -> { where(["created_at >= ?", 7.days]) }
 
 
     validates :title, length: { minimum: 1}, presence: true
-    
+
     validates :list, presence: true
     validates :user, presence: true
 end
