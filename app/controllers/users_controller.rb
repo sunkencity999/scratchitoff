b1f59ddda0fe91e2455d49f4100c27b181271897
sunkencity999 <theroyalart@gmail.com>
2016-06-24 13:48:40 -0700

    working to add delete button for user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,30 +4,30 @@ class UsersController < ApplicationController
   respond_to :json
 
   def edit
-    #defines the user as the current logged-in user. 
+    #defines the user as the current logged-in user.
     @user = current_user
   end
 
   def update_password
     #finds current user by their id, updates using devise .update_with_password() method
-    # redirects to previous page. 
+    # redirects to previous page.
     @user = User.find(current_user.id)
     if @user.update_with_password(user_params)
 
       sign_in @user, :bypass => true
-      redirect_to root_path 
+      redirect_to root_path
     else
       render "edit"
     end
   end
- 
+
   def show
     @user = User.find(params[:id])
-    
+
   end
 
   def update
-    # if the user data is update notice is flashed, and then redirects user to edit page. 
+    # if the user data is update notice is flashed, and then redirects user to edit page.
     if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
       redirect_to edit_user_registration_path(current_user)
@@ -35,9 +35,20 @@ class UsersController < ApplicationController
       render "devise/registrations/edit"
     end
   end
- 
+
+  def destroy
+    #this is the method for deleting a user account.
+    if @user.find(params[:id])
+      @user.destroy
+      respond_to do |format|
+        format.html {redirect_to welcome_index_path}
+        format.xml { head :ok}
+      end
+    end
+  end
+
    private
-    # private method to keep things RESTful and not repeat code. 
+    # private method to keep things RESTful and not repeat code.
    def user_params
      params.require(:user).permit(:name, :password, :current_password, :password_confirmation)
    end
